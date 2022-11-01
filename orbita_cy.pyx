be06a8b0c17322567e33eeba9085d48404484792
Sebasrojas01 <89663303+Sebasrojas01@users.noreply.github.com>
2022-11-01 11:03:19 -0500

    Add files via upload

--- orbita_cy.pyx
+++ orbita_cy.pyx
@@ -1,9 +1,18 @@
-#from libc.math cimport sqrt
+#cython: lenguaje_level=3
+"""
+Se requiere la raiz cuadrada:
+    Se instala como funcion externa
+    SE prepara para multihilo
+"""
+cimport cython
+
 cdef extern from "math.h":
-    double sqrt(double x)
-    
+    double sqrt(double x) nogil
+
 cdef class Planet(object):
-    cdef public double x,y,z,vx,vy,vz,m
+#Variables publicas declaracion
+
+    cdef public float x,y,z,vx,vy,vz,m
     def __init__(self):
         #some initial position and velocity
         self.x = 1.0
@@ -13,16 +22,30 @@ cdef class Planet(object):
         self.vy = 0.5
         self.vz = 0.0
 
-        # some mass
+        # doublesome mass
         self.m=1.0
 
-cdef void single_step(planet,double dt):
+"""
+Puede ser la distancia 0
+Para evitar lo anterior preparamos una alerta basada
+en cython: cdivision(True/False)
+    Al poner True, invalida la instruccion 
+    al saltar la bandera (INF)
+    
+Se prepara con un decorador de cython
+"""
+
+@cython.cdivision(True)
+cdef void single_step(Planet planet,double dt) nogil:
+
+    cdef double distance, Fx, Fy, Fz
+
     """Make a single time step"""
     #Compute force: gravity towards origin
-    cdef double distance = sqrt(planet.x**2 + planet.y**2 + planet.z**2)
-    cdef double Fx= -planet.x/distance**3
-    cdef double Fy= -planet.y/distance**3
-    cdef double Fz= -planet.z/distance**3
+    distance = sqrt(planet.x**2 + planet.y**2 + planet.z**2)
+    Fx = -planet.x/distance**3
+    Fy = -planet.y/distance**3
+    Fz = -planet.z/distance**3
 
     #Time step position, according to velocity
     planet.x += dt * planet.vx
@@ -31,13 +54,18 @@ cdef void single_step(planet,double dt):
 
     #Time step velocity, according to force and mass
     
-cdef void step_time(planet, double time_span, int n_steps):
+def step_time(Planet planet, double time_span, int n_steps):
+    cdef double dt
+    cdef int j
+    dt = time_span / n_steps
+
     """Make a number of time steps forward"""
-    cdef double dt = time_span / n_steps
+    
+    """
+    Se prepara para el paralelismo
+    """
 
-    for j in range (n_steps):
-        single_step(planet, dt)
+    with nogil:
+        for j in range (n_steps):
+            single_step(planet, dt)
 
-def main():
-    Planeta_ = Planet()
-    step_time(Planeta_, 5000,1000000)
--- principal.py
+++ principal.py
@@ -2,19 +2,57 @@ import orbita_cy
 import orbita_py
 import time
 
-ini_time = time.time()
-orbita_cy.main()
-fin_time = time.time()
+#SE inicializa el planeta en python
+tierra_py = orbita_py.Planet()
+tierra_py.x = 100*10*3
+tierra_py.y = 300*10*3
+tierra_py.z = 700*10*3
+tierra_py.vx = 2.00*10*3
+tierra_py.vy = 29.87*10*3
+tierra_py.vz = 0.034*10*3
+tierra_py.m = 5.9742*10*3
 
-time_cython = fin_time-ini_time
+#SE inicializa el planeta en cython
+tierra_cy = orbita_cy.Planet()
+tierra_cy.x = 100*10*3
+tierra_cy.y = 300*10*3
+tierra_cy.z = 700*10*3
+tierra_cy.vx = 2.00*10*3
+tierra_cy.vy = 29.87*10*3
+tierra_cy.vz = 0.034*10*3
+tierra_cy.m = 5.9742*10*3
 
-ini_time = time.time()
-orbita_py.main()
-fin_time = time.time()
+#Se consideran las otras variables
+time_span = 400
+n_steps = 2000000
 
-time_python = fin_time-ini_time
+#Definicion de experimentos 
+#Reduccion 
 
-print("Cython Time: ",time_cython)
-print("Python Time: ",time_python)
+formato_datos ="-- {:.0f}\n -- {:.1f}\n"
 
-print("Cython es: ",time_python/time_cython," mas rapido")
+for i in range(2):
+
+    ini_time = time.time()
+    orbita_cy.step_time(tierra_cy, time_span, n_steps)
+    fin_time = time.time()
+
+    time_cython = fin_time-ini_time
+
+    ini_time = time.time()
+    orbita_py.step_time(tierra_py, time_span, n_steps)
+    fin_time = time.time()
+
+    time_python = fin_time-ini_time
+
+#Se crea un formato 
+
+  
+
+    with open("planeta.csv", "w")as archivo:
+        archivo.write(formato_datos.format(i, time_python, time_cython))
+
+    print("Cython Time: ",time_cython)
+    print("Python Time: ",time_python)
+
+    print("Cython es: ",time_python/time_cython," mas rapido\n")
